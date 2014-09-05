8004249bb5dff4baeef8f6db5ee177f35f600b94
Doug Binder <doug@appfirst.com>
2014-09-05 11:41:59 -0400

    changed temp

--- thermo/main.js
+++ thermo/main.js
@@ -28,12 +28,8 @@ lineData = [];
 		var y2 = 210 * Math.sin(angle);
 
 	    lineData.push({'x1' :x1,  'y1': y1,'x2':x2, "y2" :  y2});
-
 	}
 }
-
-
-
 function createBackgroundLines(){
 
 lineData = [];
@@ -43,16 +39,12 @@ lineData = [];
 		if (angle > (1.07 + twoPi)){
 			angle = (1.07 + twoPi);
 		}
-
 		var x1 =  180 * Math.cos(angle);
 		var y1 =  180 * Math.sin(angle);
 		var x2 = 200 * Math.cos(angle);
 		var y2 = 200 * Math.sin(angle);
-
 	    lineData.push({'x1' :x1,  'y1': y1,'x2':x2, "y2" :  y2});
-
 	}
-
 svg.selectAll('.foreground').data(lineData)
 				.enter()
 				.append('line')
@@ -61,27 +53,16 @@ svg.selectAll('.foreground').data(lineData)
 				.attr('y1' , function(d){return d.y1;})
 				.attr('x2' , function(d){return d.x2;})
 				.attr('y2' , function(d){return d.y2;});
-				
-
-
 }
 
-
-
-
-
 function init(){
-	Temperature = 80; 
+	Temperature = 72;
 	width = 960;
     height = 500;
-
     progress = 0;
-var total = 1308573; // must be hard-coded if server doesn't report Content-Length
+	var total = 1308573; // must be hard-coded if server doesn't report Content-Length
     formatPercent = d3.format(".0%");
-
-lineFunction = d3.svg.line()
-
-
+	lineFunction = d3.svg.line()
 arc = d3.svg.arc()
     .startAngle(0)
     .innerRadius(180)
@@ -90,6 +71,7 @@ arc = d3.svg.arc()
 svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
+    .style("margin", "0px auto")
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 //svg.append('circle').attr('r', '65px').attr('fill', 'black')
@@ -105,10 +87,7 @@ meter.append("path")
 text = meter.append("text")
     .attr("text-anchor", "middle")
     .attr("dy", ".35em");
-
-
 createBackgroundLines();
-
 createLineData();
 drawLine();
 svg.append('text')
@@ -121,11 +100,7 @@ svg.append('text')
         .attr("x","0")
 
 drawBubbles();
-$(body).on("keypress", function(e){
 
-
-	
-})
 }
 
 
@@ -133,13 +108,10 @@ $(body).on("keypress", function(e){
 
 
 function drawBubbles(){
-
-
 	var bubble_data=[ {"cx": 4, "cy": 199, "r":8},
 					  {"cx": 15, "cy": 186, "r":6},
 					  {"cx": 3, "cy": 175, "r":7},
 					  {"cx": 13, "cy": 163, "r":5} ]
-	
 	svg.selectAll('.bubble').data(bubble_data).enter().append('circle')
 
 												.attr("class", "bubble")
@@ -152,36 +124,23 @@ function drawBubbles(){
 												.attr("r",function(d){
 																		return d.r;
 																		})
-												
-
-
-
-
-
-}
+				}
 
 function mycallback(index){
+		if (index < lineData.length){
 
+			svg.select('.tick_curr_temp[index="'+index+ '"]')
+						.transition()
 
-
-if (index < lineData.length){
-
-console.log(svg.select('.tick_curr_temp[index="'+index+ '"]').length)
-	svg.select('.tick_curr_temp[index="'+index+ '"]')
-				.transition()
-
-				.duration(function(){
-					return index*10 + 1000;
-				}).ease("elastic")
-				.attr('x2' , lineData[index]["x2"])
-				.attr('y2' , lineData[index]["y2"])
-				.each("start",function(){
-					mycallback(index+1);
-				});
-	}
-}
-
-function changetemp(){
+						.duration(function(){
+							return index*10 + 1000;
+						}).ease("elastic")
+						.attr('x2' , lineData[index]["x2"])
+						.attr('y2' , lineData[index]["y2"])
+						.each("start",function(){
+							mycallback(index+1);
+						});
+			}
 }
 
 function drawLine(){
